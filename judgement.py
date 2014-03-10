847185c5a996867c6b89ab9d180998005f0f35c5
Hackbright Student <user@mini-007.(none)>
2014-03-10 16:33:17 -0700

    Eye judgement working correctly with added random ratings.

--- judgement.py
+++ judgement.py
@@ -71,20 +71,49 @@ def view_movie(movie_id):
     user_id = session.get("user_id")
     avg, data, ratings, user_rating = model.get_movie_data(movie_id, user_id)
 
-    return render_template("movie.html", data=data, avg=avg, ratings=ratings, this_user=int(user_id), user_rating=user_rating)
+    # Prediction code: onles used if the user hasn't already rated it
+    user = model.session.query(model.User).get(session['user_id'])
+    movie = model.session.query(model.Movie).get(movie_id)
+    prediction = None
+    u_rating = None
+    if user_rating:
+        u_rating = model.session.query(model.Rating).filter_by(user_id=user_id, movie_id=movie_id).first()
+    if not u_rating:
+        prediction = user.predict_rating(movie)
+        effective_rating = prediction
+    else:
+        effective_rating = u_rating.rating
+
+    the_eye = model.session.query(model.User).filter_by(email="varchar@hackbright.com").one()
+    eye_rating = model.session.query(model.Rating).filter_by(user_id=the_eye.id, movie_id=movie.id).first()
+
+    if not eye_rating:
+        eye_rating = the_eye.predict_rating(movie)
+    else:
+        eye_rating = eye_rating.rating
+
+    if not eye_rating:
+        beratement = "What do you think?"
+    else:
+
+        difference = abs(eye_rating - effective_rating)
+
+        messages = [ "I suppose you don't have such bad taste after all. VARCHAR APPROVES!",
+                     "I regret every decision that I've ever made that has brought me to listen to your opinion.",
+                     "Words fail me, as your taste in movies has clearly failed you.",
+                     "That movie is great. For a clown to watch. Idiot."]
+
+        beratement = messages[int(difference)]
+
+    return render_template("movie.html", data=data, avg=avg, ratings=ratings, this_user=int(user_id), user_rating=user_rating, prediction=prediction, beratement=beratement)
+
 
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
 
-@app.route('/movie/<movie_id>/edit')
-def edit_rating(movie_id):
-    user_id = session.get("user_id")
-    user_rating = model.get_user_rating(movie_id, user_id)
-    return render_template("edit_rating.html", movie_id=movie_id, user_id=user_id, user_rating=user_rating)
-
-@app.route('/movie/<movie_id>/edit', methods=["POST"])
+@app.route('/movie/<movie_id>', methods=["POST"])
 def edited_rating(movie_id):
     user_id = session.get("user_id")
     user_rating = request.form.get("rating")
--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Integer, String, DATETIME
 from sqlalchemy.orm import sessionmaker, scoped_session, relationship, backref
 from operator import itemgetter
+import correlation
 # import seed
 
 ENGINE = create_engine("sqlite:///ratings.db", echo=False)
@@ -23,6 +24,40 @@ class User(Base):
     password = Column(String(64), nullable = True)
     age = Column(Integer, nullable = True)
     zipcode = Column(String(15), nullable = True)
+
+    def similarity(self, other):
+        u_ratings = {}
+        paired_ratings = []
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.rating, r.rating) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        ratings = self.ratings
+        other_ratings = movie.ratings
+        similarities = [ (self.similarity(r.user), r)\
+            for r in other_ratings ]
+        similarities.sort(reverse=True)
+        similarities = [sim for sim in similarities  if sim[0] > 0]
+        if not similarities:
+            return None
+        numerator = sum([ r.rating * similarity for similarity, r in similarities ])
+        denominator = sum([ similarity[0] for similarity in similarities ])
+        return numerator/denominator
+
+        # top_user = similarities[0]
+        # return top_user[1].rating * top_user[0]
+
+
 
 class Movie(Base):
     # creating a movie object to be added to the databse
@@ -142,6 +177,10 @@ def authenticate(user_id, password):
     else:
         return None
     pass
+
+
+
+### Correlation functions
 
 
 
